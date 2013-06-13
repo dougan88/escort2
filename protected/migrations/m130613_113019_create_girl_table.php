37e6e786e4c23b3b9716f5305747a2603c7106c0
Eugene Yurkov <eugine.yurkov@sigmaukraine.com>
2013-06-13 14:37:09 +0300

    Created migration

---
+++ protected/migrations/m130613_113019_create_girl_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130613_113019_create_girl_table extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130613_113019_create_girl_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
