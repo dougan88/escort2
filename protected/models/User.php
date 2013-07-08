1fdb05b32f21d8070153ebe4d5c48711119ddf39
Eugene Yurkov <eugine.yurkov@sigmaukraine.com>
2013-07-08 18:48:44 +0300

    Added user model

---
+++ protected/models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+class User extends CActiveRecord
+{
+
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function rules()
+    {
+        return array(
+//            // verifyCode needs to be entered correctly
+//            array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+                array('u_username, u_password, u_email', 'required'),
+                array('u_email', 'email'),
+                array('u_username', 'length', 'min' => 2, 'max' => 50),
+                array('u_username', 'length', 'min' => 6, 'max' => 50),
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+			'u_username'     => 'Name: ',
+			'u_password'     => 'Password: ',
+			'u_email'        => 'E-mail: ',
+        );
+    }
+
+    public function tableName()
+    {
+        return 'user';
+    }
+}
