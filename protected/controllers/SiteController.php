c14a33c1d749bb3f780f83f8767e9caa41525aeb
Eugene Yurkov <eugine.yurkov@sigmaukraine.com>
2013-07-22 18:39:26 +0300

    Added contact form.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -13,6 +13,7 @@ class SiteController extends Controller
 			'index'    => 'application.controllers.site.IndexAction',
 			'login'    => 'application.controllers.site.LoginAction',
 			'error'    => 'application.controllers.site.ErrorAction',
+			'contact'  => 'application.controllers.site.ContactAction',
 //			// captcha action renders the CAPTCHA image displayed on the contact page
 //			'captcha'=>array(
 //				'class'=>'CCaptchaAction',
@@ -26,27 +27,6 @@ class SiteController extends Controller
 		);
 	}
 
-//	public function actionContact()
-//	{
-//		$model=new ContactForm;
-//		if(isset($_POST['ContactForm']))
-//		{
-//			$model->attributes=$_POST['ContactForm'];
-//			if($model->validate())
-//			{
-//				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-//				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-//				$headers="From: $name <{$model->email}>\r\n".
-//					"Reply-To: {$model->email}\r\n".
-//					"MIME-Version: 1.0\r\n".
-//					"Content-type: text/plain; charset=UTF-8";
-//
-//				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-//				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-//				$this->refresh();
-//			}
-//		}
-//		$this->render('contact',array('model'=>$model));
-//	}
+
 
 }
