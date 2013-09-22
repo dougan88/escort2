0ab81f7bf248a1f236c10face536b91222036baf
dougan <dougan888@gmail.com>
2013-09-22 03:01:45 +0300

    Removed registration bug.

--- protected/controllers/site/RegisterAction.php
+++ protected/controllers/site/RegisterAction.php
@@ -14,7 +14,8 @@ class RegisterAction extends CAction
 			$user->attributes = $_POST['User'];
             if($user->validate())
             {
-				$user->u_password = crypt($user->u_password);
+				$user->u_password = crypt($user->u_password, $user->u_password);
+				$user->confirmPassword = crypt($user->confirmPassword, $user->confirmPassword);
 				$user->save();
                 Yii::app()->user->setFlash('uCreated','Successfully registered. You may <a href=' . $this->controller->createUrl('site/login') . '>login<a> now.');
 				$this->controller->refresh();
